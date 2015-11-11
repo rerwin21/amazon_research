9393c43db11835b1e96a4a72037ab8c00923053e
rerwin21 <rerwin21@users.noreply.github>
2015-11-11 09:10:41 -0500

    Adding last_step

---
+++ data_last_step.R
@@ -0,0 +1,117 @@
+# load packages -------------------------------------------------------------
+library(stringr)
+library(lubridate)
+library(plyr)
+library(dplyr)
+library(rvest)
+library(httr)
+
+# setwd ---------------------------------------------------------------------
+setwd("C:/Users/Ryan/Dropbox/RACHEL_RYAN/2_Data")
+
+
+# load the list of top reviewers --------------------------------------------
+reviewers <- read.csv("reviewers_list.csv", stringsAsFactors = F)
+
+
+# what page to scrape to for reviews
+reviewers <- reviewers %>% 
+  mutate(
+    page_num = ceiling(Reviews/10),
+    page_num = ifelse(page_num <= 10, page_num, 10)
+  )
+
+
+# grab the url for each person I'm assigned ---------------------------------
+reviewers <- reviewers %>% 
+  filter(scraper %in% c("Ryan", "Joe"),
+         page_num > 0)
+
+
+# from the number above, decide the last page to peruse of reviews ...
+# ... either 10 or floor(num reviews/10)
+create_link <- function(user, page){
+  
+  pages <- seq.int(page)
+  base_url <- "http://www.amazon.com/gp/cdp/member-reviews/"
+  base_url <- str_c(base_url, user)
+  base_url <- str_c(base_url, "?ie=UTF8&display=public&page=")
+  base_url <- paste(base_url, pages, sep = "")
+  base_url <- paste(base_url, "&sort_by=MostRecentReview", sep = "")
+  
+  return(base_url)
+}
+
+
+# using the page number and link function above, perform for all users
+create_links <- function(users, pages) {
+  
+  # create list of pages to scrape for each person
+  links <- Map(function(x, y) create_link(x, y),
+               x = users,
+               y = pages) %>% 
+    unlist()
+  
+  # get the url names
+  url_name <- names(links)
+  
+  
+  # create a data.frame
+  links <- data.frame(URLs = links,
+                      reviewer = url_name,
+                      stringsAsFactors = F,
+                      row.names = NULL)
+  
+  # return the data.frame
+  return(links)
+}
+
+
+# Review Text
+review_text <- function(link) {
+  
+  # pause to mitigate rate limit
+  pause <- runif(1, 0, 3)
+  
+  
+  # sleep
+  Sys.sleep(pause)
+
+  
+  # read html
+  # html <- read_html(httr::GET(link, use_proxy("218.200.66.196", 8080)))
+  html <- read_html(link)
+  
+  # grab the text
+  text <- html %>% 
+    html_nodes(xpath = "//div[@class = 'reviewText']") %>% 
+    html_text()
+  
+  
+  # return text
+  return(text)
+}
+  
+
+# Review Rating
+
+
+# Review Date
+
+
+# Picture: Y/N
+
+
+# Video: Y/N
+
+
+# Product Hierarchy
+
+
+# Product name
+
+
+# Product Price
+
+
+# Product discount
