8e0180d08d35da3f41d8f48fbd1b2c22b97e2410
rerwin21 <rerwin21@users.noreply.github>
2015-11-29 14:04:48 -0500

    stemming function

--- text_mining.R
+++ text_mining.R
@@ -3,6 +3,7 @@ library(plyr)
 library(dplyr)
 library(lubridate)
 library(tm)
+library(SnowballC)
 library(stringr)
 
 
@@ -31,4 +32,39 @@ total_reviews <- read.csv("total_reviews_aws.csv",
 total_reviews$review_date <- ymd(total_reviews$review_date)
 
 
-# define function for stemming and cleaning
+# define function for stemming and cleaning ---------------------------------
+sen_tok <- function(sen){
+  
+  tok <- sen %>% 
+    str_to_lower() %>% 
+    removeWords(stopwords("en")) %>% 
+    removePunctuation() %>% 
+    str_to_lower() %>% 
+    removeWords(stopwords("en")) %>% 
+    str_split(" ") %>% 
+    lapply(wordStem) %>% 
+    lapply(function(x) str_c(x, collapse = " ")) %>% 
+    unlist() %>% 
+    removeWords(stopwords("en"))
+  
+  return(tok)
+}
+
+
+# get the reviews and remove the data frame due to memory constraints -------
+review_text <- total_reviews$text
+names(review_text) <- total_reviews$review_id # in case I need the review id
+
+# remove anything except sentok function and review_text
+rm(list = setdiff(ls(), c("review_text", "sen_tok")))
+
+
+# garbage collection and clear console
+gc();cat("\014")
+
+
+# apply the function to the review text -------------------------------------
+start <- Sys.time()
+text <- lapply(review_text, sen_tok) %>% 
+  unlist()
+(end <- Sys.time() - start)
