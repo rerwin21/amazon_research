95fc9ac283ee69a6dd39d3e2a501770db29f5550
Ryan Erwin <ryan.erwin1@gmail.com>
2016-03-29 21:10:40 -0500

    amazon product api via python

---
+++ amazon_api.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 29 20:13:33 2016
+
+@author: rerwin21
+"""
+
+# In[import]
+import re, os, pandas as pd
+import amazonproduct
+
+
+# In[chg_wd]
+
+
+
+# In[load_data]
+
+
+
+# In[remove_string] 
