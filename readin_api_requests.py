13e213243603465f41c30ac07920176239b5acb8
Ryan Erwin <ryan.erwin1@gmail.com>
2016-04-13 16:29:14 -0500

    added a section to create a dataframe

--- readin_api_requests.py
+++ readin_api_requests.py
@@ -42,12 +42,28 @@ def extract(dict_in, dict_out):
 prod_item = product['api_response']['ItemLookupResponse']['Items']['Item']
 
 # get the item attributes
-prod_item['ItemAttributes']
+# prod_item['ItemAttributes']
 
 # get sales rank
-prod_item['SalesRank']
+# prod_item['SalesRank']
 
+# get flattened item attributes
+attributes_flat = extract(prod_item['ItemAttributes'], {})
 
+# add sales rank to same dict
+attributes_flat['salesrank'] = prod_item['SalesRank']
+attributes_flat['prod_id'] = prod_item['ASIN']
+
+# print out key pairs
+for key, val in attributes_flat.iteritems():
+    print "KEY: %s \n\t VAL: %s" % (key, val)
+
+# create dataframe
+df = pd.DataFrame(attributes_flat, index=[attributes_flat['prod_id']])
+
+#%% another test section
 # or just get the values for each key using the function above
 empty_dict = {} # for the function
 product_all_keyvals = extract(product, empty_dict)
+
+
