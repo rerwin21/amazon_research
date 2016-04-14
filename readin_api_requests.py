441d93070a8bce5041e10654c6b77df069238159
Ryan Erwin <ryan.erwin1@gmail.com>
2016-04-13 23:59:08 -0500

    added functions and streamlined the rest

--- readin_api_requests.py
+++ readin_api_requests.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 13 13:17:51 2016
+This script defines a few s
 
 @author: rerwin21
 """
@@ -8,97 +8,68 @@ Created on Wed Apr 13 13:17:51 2016
 
 #%% import my libraries
 import pandas as pd
-import numpy as np
-import os
 import json
 
 
 #%% where is my data?
 data_file = '/home/rerwin21/amazon_proj/api_data/api_request_3.txt'
 
-with open(data_file) as api_data:
-    first_line = api_data.readline()
-    
 
-#%% load json string as python object
-
-product = json.loads(first_line)
-
-
-#%% dict function, taken and altered slightly from StackOverflow
+#%% dict function, taken and altered from StackOverflow to iinclude the 
+# test for list
 def extract(dict_in, dict_out):
     for key, value in dict_in.iteritems():
         if isinstance(value, dict): # If value itself is dictionary
             extract(value, dict_out)
+        elif isinstance(value, list):
+            for i, j in enumerate(value):
+                if i == 0:
+                    keys = key
+                else:
+                    keys = '_'.join([key, str(i + 1)])
+                dict_out[keys] = j
+            count_key = '_'.join([key, 'count'])
+            dict_out[count_key] = len(value)
         else:
-            # Write to dict_out
             dict_out[key] = value
     return dict_out
 
-    
-#%% notes to parse the JSON
-
-# gets the actual item (none of the garbage)
-prod_item = product['api_response']['ItemLookupResponse']['Items']['Item']
-
-# get the item attributes
-# prod_item['ItemAttributes']
-
-# get sales rank
-# prod_item['SalesRank']
-
-# get flattened item attributes
-attributes_flat = extract(prod_item['ItemAttributes'], {})
-
-# add sales rank to same dict
-attributes_flat['salesrank'] = prod_item['SalesRank']
-attributes_flat['prod_id'] = prod_item['ASIN']
-
-# print out key pairs
-for key, val in attributes_flat.iteritems():
-    print "KEY: %s\n\t  VAL: %s\n" % (key, val)
-
-# create dataframe
-df = pd.DataFrame(attributes_flat, index=[attributes_flat['prod_id']])
-
-#%% another test section
-# or just get the values for each key using the function above
-empty_dict = {} # for the function
-product_all_keyvals = extract(product, empty_dict)
-
-
-#%%
-
-
-with open(data_file) as myfile:
-    head = [next(myfile) for x in xrange(2)]
-   
+     
 #%%   
 # use head as test file object   
 def product_attributes(prod_json):
+    attributes_flat = {}    
     product = json.loads(prod_json)
     
     # get the itam    
-    prod_item = product['api_response']['ItemLookupResponse']['Items']['Item']
+    if 'Item' in product['api_response']['ItemLookupResponse']['Items']:
+        prod_item = product['api_response']['ItemLookupResponse']['Items']['Item']
 
-    # get flattened item attributes
-    attributes_flat = extract(prod_item['ItemAttributes'], {})
-
-    # add sales rank and product id to same dict
-    if 'SalesRank' in prod_item:
-        attributes_flat['salesrank'] = prod_item['SalesRank']
-        
-    attributes_flat['prod_id'] = prod_item['ASIN']
-
-    # create dataframe
-    df = pd.DataFrame(attributes_flat, index=[attributes_flat['prod_id']])
+        # get flattened item attributes
+        attributes_flat = extract(prod_item['ItemAttributes'], {})
+ 
+        if 'SalesRank' in prod_item:
+            attributes_flat['salesrank'] = prod_item['SalesRank']
+  
+    attributes_flat['prod_id'] = product['product_asin']
     
+    df = pd.DataFrame(attributes_flat, index=[product['product_asin']])    
     return df
     
+    
 #%%
-
+# get the first 100 products and put them in a list
+with open(data_file) as myfile:
+    head = [next(myfile) for x in xrange(101)]
+    
+    
+#%% input to DataFrame
 prod_df = pd.DataFrame()
-
 for prod in head:
     df = product_attributes(prod)
     prod_df = prod_df.append(df)            
+    
+    
+#%% write to disk
+# make sure to change the current directory
+prod_df.to_csv('api_request_100', index=False, encoding='utf-8')
